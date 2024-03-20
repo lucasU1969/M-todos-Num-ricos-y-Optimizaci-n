be98c56b0d2a8cc2020001a69ff8f875e04b38ce
lucasU1969 <lromanoabdo@udesa.edu.ar>
2024-03-20 13:12:02 -0300

    Funciones de error Agregué las funciones de error para calcular el error más grande. fb era para probar Traté de hacer el lagrange con scipy pero da cualquier cosa Está el gráfico de la función fa

--- tp01_ej01a.py
+++ tp01_ej01a.py
@@ -1,8 +1,39 @@
+from scipy.interpolate import lagrange
 import numpy as np
+import matplotlib.pyplot as plt
 
 inicio = -4  # Valor inicial del intervalo
 fin = 4    # Valor final del intervalo
 numero_elementos = 100  # Número de elementos equiespaciados
 
-lista_equiespaciada = np.linspace(inicio, fin, numero_elementos)
-print(lista_equiespaciada)
+# crea la lista de elementos equiespaciados
+def fa(x:float) -> float:
+    return (0.3**abs(x))*np.sin(4*x) - np.tanh(2*x) + 2
+
+def fb(x: float) -> float:
+    return 1
+
+def error_absoluto(f1, f2, intervalo:list) -> list:
+    return f1(intervalo) - f2(intervalo)
+
+def error_absoluto_máximo( f1, f2, intervalo:list) -> float:
+    return max(error_absoluto(f1,f2, intervalo))
+
+
+coords_x = np.linspace(inicio, fin, numero_elementos)
+coords_y = fa(coords_x)
+
+# print(coords_y[::2])
+
+# coords_x2 = coords_x[::2]
+# coords_y2 = coords_y[::2]
+
+# pol_lagrange = lagrange( coords_x2, coords_y2)
+
+# print( error_absoluto_máximo(fa, pol_lagrange, coords_x))
+
+plt.plot(coords_x, coords_y)
+# plt.plot(coords_x2, coords_y2)
+
+plt.show()
+
