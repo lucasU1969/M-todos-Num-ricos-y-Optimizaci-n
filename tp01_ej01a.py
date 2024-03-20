2e9adfcdb83effb11e43c851da217fc03142313f
lucasU1969 <lromanoabdo@udesa.edu.ar>
2024-03-20 11:48:26 -0300

    Lista de números equiespaciados en el intervalo
    Uso numpy.linspace() para crear una lista de 100 números equiespaciados en
    el intervalo -4, 4.
    
    Ejercicio1a.

---
+++ tp01_ej01a.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+inicio = -4  # Valor inicial del intervalo
+fin = 4    # Valor final del intervalo
+numero_elementos = 100  # Número de elementos equiespaciados
+
+lista_equiespaciada = np.linspace(inicio, fin, numero_elementos)
+print(lista_equiespaciada)
